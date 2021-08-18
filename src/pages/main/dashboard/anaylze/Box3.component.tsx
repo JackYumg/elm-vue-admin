5a2092f23d362c81be49a00d136c072276736503
wethin <806769481@qq.com>
2021-08-18 19:47:11 +0800

    fix:修改

--- src/pages/main/dashboard/anaylze/Box3.component.tsx
+++ src/pages/main/dashboard/anaylze/Box3.component.tsx
@@ -1,7 +1,26 @@
+import { ElCard, ElCol, ElIcon, ElRow } from "element-plus";
 import { defineComponent } from "vue";
 
 export default defineComponent({
     render() {
-        return <div>1212</div>
+        const title1 = () => {
+            return <div>线上热门搜索
+                <ElIcon name={'more'}></ElIcon>
+            </div>
+        }
+
+        const slots1 = {
+            header: title1
+        }
+        return <ElRow gutter={20}>
+            <ElCol span={12}>
+                <ElCard v-slots={slots1}>
+
+                </ElCard>
+            </ElCol>
+            <ElCol span={12}>
+                <ElCard></ElCard>
+            </ElCol>
+        </ElRow>
     }
 })
